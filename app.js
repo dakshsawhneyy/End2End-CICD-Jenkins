5072728868aa898aa7a33c0d35306363d9198e0b
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-04-14 08:27:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+    res.send('Hello from Daksh!');
 });
 
 app.listen(3000, () => {
